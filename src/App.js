e8810e43c9b45b010fb081dee44010692de3645c
Step_atlas <stp.sip7@gmail.com>
2023-02-28 14:24:00 -0500

    page + components

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Footer, Tours, Services, About, Hero, Navbar } from "./components";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import About from "./About";
+import Hero from "./Hero";
+import Services from "./Services";
+import Tours from "./Tours";
+import Footer from "./Footer";
+import Navbar from "./Navbar"
+
+export { About, Hero, Services, Tours, Navbar, Footer };
